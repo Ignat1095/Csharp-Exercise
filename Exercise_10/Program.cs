11af5c50314d85148cb5842d2271fe11f33fe9c1
Ignat <Ignat1095@mail.ru>
2022-08-11 19:14:15 +0300

    Ex_10 Done

--- Exercise_10/Program.cs
+++ Exercise_10/Program.cs
@@ -5,3 +5,38 @@
 // 782 -> 8
 // 918 -> 1
 
+
+Console.WriteLine();
+Console.WriteLine();
+
+//456 --> 5
+int a = 456;
+int a_10 = a % 100; //-->56
+int b_ = a_10 / 10; //-->5
+
+Console.WriteLine("Second digit of " + a + " will be: " + b_);
+
+Console.WriteLine();
+Console.WriteLine();
+//782 --> 8
+Console.WriteLine("Second digit of " + 782 + " will be: " + 782 / 10 % 10);
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+
+// 918 --> 1 or anythink random number
+Console.Write("input three-digit number (fron 100 before 999) : ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+if ((j / 100 < 0) ^ (j / 100 < 10))
+{
+    Console.WriteLine("Second digit " + j + " will be: " + (j / 10 % 10));
+}
+else
+{
+    Console.WriteLine("You input not three-digit number");
+}
+
+
