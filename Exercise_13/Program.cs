27e4d9ff83a00bd7e7499fcc6980a9bd47407c95
Ignat <Ignat1095@mail.ru>
2022-08-11 20:17:56 +0300

    Ex_13 Done

--- Exercise_13/Program.cs
+++ Exercise_13/Program.cs
@@ -4,3 +4,33 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+// Console.Write("input three-digit number (fron 100 before 999) : ");
+// int j = Convert.ToInt32(Console.ReadLine());
+
+// if ((99 < j) ^ (j > 999)) // (j / 100 < 0) ^ (j / 100 < 10)
+// {
+//     Console.WriteLine("Second digit " + j + " will be: " + (j / 10 % 10));
+// }
+// else
+// {
+//     Console.WriteLine("You input not three-digit number");
+// }
+
+
+
+Console.Clear();
+
+Console.Write("Input Number: ");
+int any_numb = Convert.ToInt32(Console.ReadLine());
+
+String any_numb_text = Convert.ToString(any_numb);
+
+if (any_numb_text.Length > 2)
+{
+    Console.WriteLine("third digit will be: " + any_numb_text[2]);
+}
+else
+{
+    Console.WriteLine("no third digit");
+}
