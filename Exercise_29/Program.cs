3de98245964fbdcf6da92725ef255c05e2aea0e1
Ignat <Ignat1095@mail.ru>
2022-08-18 20:08:23 +0300

    Ex_29 - Done

--- Exercise_29/Program.cs
+++ Exercise_29/Program.cs
@@ -3,3 +3,29 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+
+Console.Write("Введите длину массива: ");
+int[] myArray = new int[int.Parse(Console.ReadLine())];
+
+Console.Write("Введите максимальное число массива: ");
+int digit = int.Parse(Console.ReadLine());
+
+Random rand = new Random();
+
+Console.Write("Ваш массив: [");
+
+for (int i = 0; i < myArray.Length; i++) //Код массива
+{
+
+    if (i < (myArray.Length - 1))
+    {
+        myArray[i] = rand.Next(digit);
+        Console.Write(myArray[i] + ", ");
+    }
+    else
+    {
+        myArray[i] = rand.Next(digit);
+        Console.Write(myArray[i] + "]");
+    }
+}
