19496fea899472f4392a7841db21cec287ca21cc
Ignat <Ignat1095@mail.ru>
2022-08-22 23:13:22 +0300

    Ex_34 - Done

---
+++ Ex_30_МассивЗаполненый_0_и_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Массив из 8  заполненный 0 и 1
+
+int[] array = new int[8];
+
+/*int i = 0;
+
+while (i < array.Length)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write(array[i]);
+    i++;
+}
+*/
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write(array[i]);
+}
---
+++ Ex_34_ПоискЧетныхЧиселВМассиве/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] array = new int[10];
+
+Console.Write("[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write(array[i]);
+    Console.Write(" ");
+
+
+}
+Console.WriteLine("]");
+
+int count = 0; //переменная в которой будет храниться ко-во четных значений
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+
+    }
+
+}
+Console.WriteLine("Кол-во четных значений в массиве = " + count);
+
+if (count == 10)
+{
+    Console.WriteLine("Вау! Да у нас джек-пот!!! =D");
+}
+if (count == 0)
+{
+    Console.WriteLine("Ну ничего страшного... Тяу тяу тяу тяу...");
+}
+
+
