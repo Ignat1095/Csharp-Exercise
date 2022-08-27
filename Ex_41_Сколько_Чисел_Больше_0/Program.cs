be7ba39aee749ed424f56c90aab5cffcebb298ad
Ignat <Ignat1095@mail.ru>
2022-08-27 14:35:08 +0300

    obj

---
+++ Ex_41_Сколько_Чисел_Больше_0/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(" ");
+    }
+}
+
+Console.Write("Кол-во элементов массива: ");
+int ElemetnsCount = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = new int[ElemetnsCount];
+
+
+FillArray(myArray);
+
+Console.Write("\n Наш массив: [ ");
+PrintArray(myArray);
+Console.WriteLine("]");
+
+int sum = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > 0) sum++;
+}
+Console.WriteLine($"Положительных чисел - {sum} штук.");
+
---
+++ Ex_43_Точка_Пересечения/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями:
+
+
+// y = k1 * x + b1, 
+// y = k2 * x + b2; 
+
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, 
+// k1 = 5, 
+// b2 = 4, 
+// k2 = 9  
+
+// --> (-0,5; -0,5)
+
+
+
+// k1 * x + b1 = k2 * x + b2    --> (k1 - k2) * x = b2 - b1    -->                                 
+// x = (b2 - b1) / (k1 - k2)
+
+
+Console.Write("Введите переменную b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите коэффициент k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("\nВведите переменную b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите коэффициент k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double x = ((b2 - b1) / (k1 - k2)); // or x = ((b1 - b2) / (k2 - k1));
+double y = k1 * x + b1;
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны и не пересекутся!");
+}
+else
+{
+    Console.Write("\nТочка Пересечения: ");
+    Console.Write("(" + x + ";" + y + ")");
+}
